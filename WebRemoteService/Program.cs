bfeb03e1100c6e52d3173708a17665880b1b5db4
Fatlum Latifi <fatlumlatif@gmail.com>
2025-03-10 12:26:06 +0100

    ..

--- WebRemoteService/Program.cs
+++ WebRemoteService/Program.cs
@@ -1,13 +1,9 @@
 using Microsoft.Extensions.Hosting;
 using ServiceRelease;
+var builder = Host.CreateApplicationBuilder();
 
-
-var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddSystemd();
-
 builder.Services.AddHostedService<Worker>();
-
 var host = builder.Build();
-
 
 host.Run();
